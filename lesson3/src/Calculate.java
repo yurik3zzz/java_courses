67c302d496bc885ad4ac827216af6dbbb096f233
yurik3zzz <yurik3zzz@gmail.com>
2016-03-18 16:55:42 +0200

    First empty class

--- lesson3/src/Calculate.java
+++ lesson3/src/Calculate.java
@@ -3,4 +3,12 @@
  */
 public class Calculate {
 
+    /**
+     * File->Settings->Live Templates: other там собственно psvm
+     * psvm+TAB
+     * sout+TAB
+     */
+    public static void main(String[] args) {
+        System.out.println("Calculate.main");
+    }
 }
